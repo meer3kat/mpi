b8ddbf35250e130cb10e86d79ee6647d26c1bbc6
meer3kat <peiliguo@gmail.com>
2019-05-09 22:21:01 +0200

    qsort

--- a2/qsort.c
+++ a2/qsort.c
@@ -396,13 +396,14 @@ int main(int argc, char *argv[]){
 		MPI_Barrier(MPI_COMM_WORLD); 
 
 	}
-	
+
 	MPI_Bcast(sorted_array, n2, MPI_INT, 0, MPI_COMM_WORLD);
 
 	if(rank==1){
 		print_array(sorted_array,n2);
 		save_result(output_file, sorted_array, n2);
 	}
+	MPI_Barrier(MPI_COMM_WORLD); 
 	// printf("finished saving\n");
 	if(collect_done == 1){free(local_arr);}
 	// if(rank==1){print_array(local_arr, local_size);}
