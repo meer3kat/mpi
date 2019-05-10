99c797f000ecee45edef489b2f2050604c836b6c
meer3kat <peiliguo@gmail.com>
2019-05-10 17:03:03 +0200

    add .sh files

--- a2/qsort_2.sh
+++ a2/qsort_2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 #SBATCH -A g2019005
-#SBATCH -t 20:00
-#SBATCH -p core -n 16
+#SBATCH -t 10:00
+#SBATCH -p core -n 2
 
 module load gcc openmpi 
-mpirun -np 16 ./qsort /proj/g2019005/nobackup/qsort_indata/input125000000.txt outputa.txt 0
+mpirun -np 2 ./qsort /proj/g2019005/nobackup/qsort_indata/input125000000.txt outputa.txt 0
+mpirun -np 2 ./qsort /proj/g2019005/nobackup/qsort_indata/input125000000.txt outputa.txt 1
+mpirun -np 2 ./qsort /proj/g2019005/nobackup/qsort_indata/input125000000.txt outputa.txt 2
 
-input1000000000.txt
-iinput125000000.txt
---
+++ a2/qsort_8.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+#SBATCH -A g2019005
+#SBATCH -t 10:00
+#SBATCH -p core -n 8
+
+module load gcc openmpi 
+mpirun -np 8 ./qsort /proj/g2019005/nobackup/qsort_indata/input125000000.txt outputa.txt 0
+mpirun -np 8 ./qsort /proj/g2019005/nobackup/qsort_indata/input125000000.txt outputa.txt 1
+mpirun -np 8 ./qsort /proj/g2019005/nobackup/qsort_indata/input125000000.txt outputa.txt 2
+
