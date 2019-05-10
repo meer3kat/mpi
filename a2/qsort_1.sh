2951926cec5b3cc0341879c17f66cee521f4e6b0
meer3kat <peiliguo@gmail.com>
2019-05-10 17:21:42 +0200

    run 500000000

--- a2/qsort_1.sh
+++ a2/qsort_1.sh
@@ -4,7 +4,7 @@
 #SBATCH -p core -n 1
 
 module load gcc openmpi 
-mpirun -np 1 ./qsort /proj/g2019005/nobackup/qsort_indata/input125000000.txt outputa.txt 0
-mpirun -np 1 ./qsort /proj/g2019005/nobackup/qsort_indata/input125000000.txt outputa.txt 1
-mpirun -np 1 ./qsort /proj/g2019005/nobackup/qsort_indata/input125000000.txt outputa.txt 2
+mpirun -np 1 ./qsort /proj/g2019005/nobackup/qsort_indata/input500000000.txt outputa.txt 0
+mpirun -np 1 ./qsort /proj/g2019005/nobackup/qsort_indata/input500000000.txt outputa.txt 1
+mpirun -np 1 ./qsort /proj/g2019005/nobackup/qsort_indata/input500000000.txt outputa.txt 2
 
