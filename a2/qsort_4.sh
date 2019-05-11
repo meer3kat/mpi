3ef05bdcf91183ff49afbbd0de140310658af796
meer3kat <peiliguo@gmail.com>
2019-05-11 19:37:56 +0200

    qsort_4.sh

--- a2/qsort_4.sh
+++ a2/qsort_4.sh
@@ -4,7 +4,8 @@
 #SBATCH -p core -n 4
 
 module load gcc openmpi 
-mpirun -np 4 ./qsort /proj/g2019005/nobackup/qsort_indata/backwards125000000.txt outputa.txt 0
-mpirun -np 4 ./qsort /proj/g2019005/nobackup/qsort_indata/backwards125000000.txt outputa.txt 1
+mpirun -np 4 ./qsort /proj/g2019005/nobackup/qsort_indata/input93.txt outputa.txt 0
+mpirun -np 4 ./qsort /proj/g2019005/nobackup/qsort_indata/backwards93.txt outputa.txt 0
+mpirun -np 4 ./qsort /proj/g2019005/nobackup/qsort_indata/input2000000000.txt outputa.txt 1
 mpirun -np 4 ./qsort /proj/g2019005/nobackup/qsort_indata/backwards125000000.txt outputa.txt 2
 
