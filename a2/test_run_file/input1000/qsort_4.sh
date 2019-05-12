c03f0b875fbe210ef2620abe43fb40e3ab284fb6
meer3kat <peiliguo@gmail.com>
2019-05-12 14:56:31 +0200

    ready to run input1000

---
+++ a2/test_run_file/input1000/qsort_4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+#SBATCH -A g2019005
+#SBATCH -t 25:00
+#SBATCH -p core -n 4
+
+module load gcc openmpi 
+mpirun -np 4 ./qsort /proj/g2019005/nobackup/qsort_indata/input1000000000.txt outputb.txt 0
+mpirun -np 4 ./qsort /proj/g2019005/nobackup/qsort_indata/input1000000000.txt outputb.txt 1
+mpirun -np 4 ./qsort /proj/g2019005/nobackup/qsort_indata/input1000000000.txt outputb.txt 2
+
