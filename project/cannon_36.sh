9662868dab242897a1567c36f68f4bd358af5c91
meer3kat <peiliguo@gmail.com>
2019-06-09 20:03:49 +0200

    add .sh file for run

---
+++ project/cannon_36.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#SBATCH -A g2019005
+#SBATCH -t 1000:00
+#SBATCH -p node -n 36
+
+
+module load gcc openmpi 
+mpirun -np 36 ./mm_cannon /proj/g2019005/nobackup/matmul_indata/input3600.txt output.txt
+mpirun -np 36 ./mm_cannon /proj/g2019005/nobackup/matmul_indata/input7488.txt output.txt
+mpirun -np 36 ./mm_cannon /proj/g2019005/nobackup/matmul_indata/input9072.txt output.txt
+
+
